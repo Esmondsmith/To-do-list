e5b4b8c5222c8daa6137dec19ed1043a2999a535
Esmondsmith <133409203+Esmondsmith@users.noreply.github.com>
2023-12-07 15:33:59 +0100

    made changes on the arrow function for e.target.value

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,8 +5,13 @@ const TodoForm = ({addTodo}) => {
   const [value, setValue] = useState ('')
 
   const handleSubmit = e => {
-    e.preventDefault(); //To prevent default action of the form
-
+    e.preventDefault(); //To prevent default action of the form.
+    // if(value === ""){
+    //   alert("please enter a task")
+    // } else {
+    //   console.log({value})
+    //   alert(`${value} has been added to the list`);
+    // }
     addTodo(value)
     //This is used to clear the input field after the value entered is submitted
     setValue ('')
@@ -15,7 +20,7 @@ const TodoForm = ({addTodo}) => {
   return (
     <form className='TodoForm' onSubmit={handleSubmit}>
       {/* To get the user input, we add the onChange event to the input field*/}
-      <input type='text' placeholder='Task to do today' className='todo-input' value={value} onChange={(e) => setValue(e.target.value)}/>
+      <input type='text' placeholder='Task to do today' className='todo-input' value={value} onChange={(e) => {setValue(e.target.value)}}/>
       <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
